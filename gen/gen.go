fbb36e574e6d7e123ebf61236c6d5239f4083934
Nanite Factory <nanite@naver.com>
2019-12-10 01:31:41 +0900

    less loc

--- gen/gen.go
+++ gen/gen.go
@@ -1,10 +1,14 @@
-// Package gen provides various template-based source code generators for the
-// Chrome DevTools Protocol domain definitions.
 package gen
 
 import (
 	"bytes"
+	"path"
+	"path/filepath"
 
+	"github.com/nanitefactory/chromebot-domain-gen/gen/gotpl"
+	"github.com/valyala/quicktemplate"
+
+	"github.com/chromedp/cdproto-gen/gen/genutil"
 	"github.com/chromedp/cdproto-gen/pdl"
 )
 
@@ -21,4 +25,81 @@ func Generators() map[string]Generator {
 	return map[string]Generator{
 		"go": NewGoGenerator,
 	}
+}
+
+// GoGenerator generates Go source code for the Chrome DevTools Protocol.
+type GoGenerator struct {
+	files fileBuffers
+}
+
+// NewGoGenerator creates a Go source code generator for the Chrome DevTools
+// Protocol domain definitions.
+func NewGoGenerator(domains []*pdl.Domain, outBasePkg string) (Emitter, error) {
+	var w *quicktemplate.Writer
+
+	fb := make(fileBuffers)
+
+	// generate individual domains
+	for _, d := range domains {
+		pkgName := genutil.PackageName(d)
+		pkgOut := filepath.Join(pkgName, pkgName+".go")
+
+		// do command template
+		w = fb.get(pkgOut, pkgName, d, domains, "github.com/chromedp/cdproto")
+		gotpl.StreamDomainTemplate(w, d, domains)
+		fb.release(w)
+	}
+
+	return &GoGenerator{
+		files: fb,
+	}, nil
+}
+
+// Emit returns the generated files.
+func (gg *GoGenerator) Emit() map[string]*bytes.Buffer {
+	return map[string]*bytes.Buffer(gg.files)
+}
+
+// fileBuffers is a type to manage buffers for file data.
+type fileBuffers map[string]*bytes.Buffer
+
+// get retrieves the file buffer for s, or creates it if it is not yet available.
+func (fb fileBuffers) get(s string, pkgName string, d *pdl.Domain, domains []*pdl.Domain, basePkg string) *quicktemplate.Writer {
+	// check if it already exists
+	if b, ok := fb[s]; ok {
+		return quicktemplate.AcquireWriter(b)
+	}
+
+	// create buffer
+	b := new(bytes.Buffer)
+	fb[s] = b
+	w := quicktemplate.AcquireWriter(b)
+
+	v := d
+	if b := path.Base(s); b != pkgName+".go" {
+		v = nil
+	}
+
+	// add package header
+	gotpl.StreamFileHeader(w, pkgName, v)
+
+	// add import map
+	importMap := map[string]string{
+		"encoding/json":                      "",
+		basePkg + "/cdp":                     "",
+		"github.com/mailru/easyjson":         "",
+		"github.com/mailru/easyjson/jlexer":  "",
+		"github.com/mailru/easyjson/jwriter": "",
+	}
+	for _, d := range domains {
+		importMap[basePkg+"/"+genutil.PackageName(d)] = ""
+	}
+	gotpl.StreamFileImportTemplate(w, importMap)
+
+	return w
+}
+
+// release releases a template writer.
+func (fb fileBuffers) release(w *quicktemplate.Writer) {
+	quicktemplate.ReleaseWriter(w)
 }
